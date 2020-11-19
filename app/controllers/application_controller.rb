ca4b0b41106e6090f2feb3ca27720f1124611f7e
Nakajima3106 <uver777.333@gmail.com>
2020-11-19 07:30:45 +0000

    テスト前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
-  
-  
+# アクセス制限
+  before_action :authenticate_user!,except: [:top, :about]
+
+  # devise使用時にメソッドを呼ぶ
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
+  # ログイン後の転送先を指定
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
   protected
   def configure_permitted_parameters
     # ユーザ登録時にユーザ名(name) の操作を許可する
