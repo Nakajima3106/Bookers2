bb760acf0fb666fe00270b3b2dbf0457734c40b8
Nakajima3106 <uver777.333@gmail.com>
2020-11-22 04:55:04 +0000

    画像投稿チェック

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e0dd960a63e29d402126ac7c1268234e3f95ebf92f57f97defe84f595266687f8d5d369b51c95dab47e515c6fa5a31ae7f55d45147772e81910baa741e535f81'
